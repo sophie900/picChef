28726b3811e153904d69bf4eb85dadcf9bf97b68
Sophie Rong <sophie.rong@gmail.com>
2025-07-31 12:32:54 -0700

    Implement BeautifulSoup recipe scraper

---
+++ backend/scrape_recipes.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def scrape_recipe(url: str) -> list:
+    """
+    Scrapes recipes from the given URL. Returns a dictionary with details
+    about the specified number of recipes.
+    """
+    try:
+        # Retrieve the page content
+        response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError if one occurred
+
+        # Create the soup
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Scrape card info for all recipe cards within the container
+        card_container = soup.find(id='mntl-search-results__list_1-0')
+
+        all_recipes = []
+
+        for card in card_container.children:
+            if (card.name == 'a') and ('href' in card.attrs):
+                all_recipes.append({
+                    'recipe_name': card.select_one('.card__content .card__title').text.strip(),
+                    'recipe_link': card['href'],
+                    'recipe_image': card.select_one('.card__top .card__media .img-placeholder img')['data-src']
+                })
+
+        return all_recipes
+    except Exception as e:
+        print(f'Error scraping recipes: {e}')
+        return {'error': str(e)}
